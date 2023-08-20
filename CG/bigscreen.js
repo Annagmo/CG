af451559910b9824899326d122096e0cbc5232c4
Anna G <85114312+Annagmo@users.noreply.github.com>
2023-08-20 19:37:04 -0300

    Add files via upload

---
+++ CG/bigscreen.js
@@ -0,0 +1,492 @@
+﻿"use strict";
+
+// This is not a full .obj parser.
+// see http://paulbourke.net/dataformats/obj/
+
+function parseOBJ(text) {
+  // because indices are base 1 let's just fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
+
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
+
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
+
+  const materialLibs = [];
+  const geometries = [];
+  let geometry;
+  let groups = ['default'];
+  let material = 'default';
+  let object = 'default';
+
+  const noop = () => {};
+
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+  }
+
+  function setGeometry() {
+    if (!geometry) {
+      const position = [];
+      const texcoord = [];
+      const normal = [];
+      webglVertexData = [
+        position,
+        texcoord,
+        normal,
+      ];
+      geometry = {
+        object,
+        groups,
+        material,
+        data: {
+          position,
+          texcoord,
+          normal,
+        },
+      };
+      geometries.push(geometry);
+    }
+  }
+
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      setGeometry();
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
+    s: noop,    // smoothing group
+    mtllib(parts, unparsedArgs) {
+      // the spec says there can be multiple filenames here
+      // but many exist with spaces in a single filename
+      materialLibs.push(unparsedArgs);
+    },
+    usemtl(parts, unparsedArgs) {
+      material = unparsedArgs;
+      newGeometry();
+    },
+    g(parts) {
+      groups = parts;
+      newGeometry();
+    },
+    o(parts, unparsedArgs) {
+      object = unparsedArgs;
+      newGeometry();
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  // remove any arrays that have no entries.
+  for (const geometry of geometries) {
+    geometry.data = Object.fromEntries(
+        Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+  }
+
+  return {
+    geometries,
+    materialLibs,
+  };
+}
+
+async function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector("#canvas");
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  // Tell the twgl to match position with a_position etc..
+  twgl.setAttributePrefix("a_");
+
+  const vs = `#version 300 es
+  in vec4 a_position;
+  in vec3 a_normal;
+  in vec2 a_texcoord;
+
+  uniform mat4 u_projection;
+  uniform mat4 u_view;
+  uniform mat4 u_world;
+
+  out vec3 v_normal;
+  // a varying to pass the texture coordinates to the fragment shader
+  out vec2 v_texcoord;
+
+  void main() {
+    gl_Position = u_projection * u_view * u_world * a_position;
+    v_normal = mat3(u_world) * a_normal;
+    // Pass the texcoord to the fragment shader.
+    v_texcoord = a_texcoord;
+  }
+  `;
+
+  const fs = `#version 300 es
+  precision highp float;
+
+  in vec3 v_normal;
+  in vec2 v_texcoord;
+
+  uniform vec4 u_diffuse;
+  uniform vec3 u_lightDirection;
+  // The texture.
+  uniform sampler2D u_texture;
+
+  out vec4 outColor;
+
+  void main () {
+    vec3 normal = normalize(v_normal);
+    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+    outColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
+    //outColor = texture(u_texture, v_texcoord);
+
+  }
+  `;
+
+
+  // compiles and links the shaders, looks up attribute and uniform locations
+  const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+  //ANNA: FILE, RESPONSE, TEXT E OBJ dos 3 objetos 
+  const firstObjFile = "lighthouse.obj"; 
+  const secondObjFie = "turtle.obj"; 
+  const thirdObjFile = "hammer.obj"; 
+
+  const firstObjRes = await fetch(firstObjFile); 
+  const secondObjRes = await fetch(secondObjFie); 
+  const thirdObjRes = await fetch(thirdObjFile); 
+
+  const firstObjText = await firstObjRes.text();
+  const secondObjText = await secondObjRes.text();
+  const thirdObjText = await thirdObjRes.text();
+
+  const obj = parseOBJ(firstObjText);
+  const secondObj = parseOBJ(secondObjText);
+  const thirdObj = parseOBJ(thirdObjText);
+
+
+  //ANNA: PARTS primeiro objeto
+  const parts = obj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+    return {
+      material: {
+        u_diffuse: [Math.random(), Math.random(), Math.random(), 1],
+      },
+      bufferInfo,
+      vao,
+    };
+  });
+
+  //ANNA: PARTS segundo objeto
+  const PartsSecondOBJ = secondObj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+    return {
+      material: {
+        u_diffuse: [Math.random(), Math.random(), Math.random(), 1],
+      },
+      bufferInfo,
+      vao,
+    };
+  });
+
+    //ANNA: PARTS terceiro objeto
+  const PartsThirdOBJ = thirdObj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+    return {
+      material: {
+        u_diffuse: [Math.random(), Math.random(), Math.random(), 1],
+      },
+      bufferInfo,
+      vao,
+    };
+  });
+
+
+  function getExtents(positions) {
+    const min = positions.slice(0, 3);
+    const max = positions.slice(0, 3);
+    for (let i = 3; i < positions.length; i += 3) {
+      for (let j = 0; j < 3; ++j) {
+        const v = positions[i + j];
+        min[j] = Math.min(v, min[j]);
+        max[j] = Math.max(v, max[j]);
+      }
+    }
+    return {min, max};
+  }
+
+  function getGeometriesExtents(geometries) {
+    return geometries.reduce(({min, max}, {data}) => {
+      const minMax = getExtents(data.position);
+      return {
+        min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+        max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+      };d
+    }, {
+      min: Array(3).fill(Number.POSITIVE_INFINITY),
+      max: Array(3).fill(Number.NEGATIVE_INFINITY),
+    });
+  }
+
+  const extents = getGeometriesExtents(obj.geometries);
+  const range = m4.subtractVectors(extents.max, extents.min);
+  // amount to move the object so its center is at the origin
+  const objOffset = m4.scaleVector(
+      m4.addVectors(
+        extents.min,
+        m4.scaleVector(range, 0.5)),
+      -1);
+
+  const cameraTarget = [0, 0, 0];
+  // figure out how far away to move the camera so we can likely
+  // see the object.
+  const radius = m4.length(range) * 1.2;
+  const cameraPosition = m4.addVectors(cameraTarget, [
+    radius,
+    0,
+    0,
+  ]);
+  
+  //---- ANNA: ATUALIZAÇÃO DA CAMERA -----
+  //cameraPosition[0] = ponto no eixo X
+  //cameraPosition[1] = ponto no eixo Y
+
+
+  // Set zNear and zFar to something hopefully appropriate
+  // for the size of this object.
+  const zNear = radius / 100;
+  const zFar = radius * 3;
+
+  function degToRad(deg) {
+    return deg * Math.PI / 180;
+  }
+
+  //----ANNA: MOVIMENTAÇÃO TURTLE E SHARK----
+  var shark = 0;//FORA DO RENDER!!!!!!
+  var turtle = 0;
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.enable(gl.DEPTH_TEST);
+
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+
+    const up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+
+    // Make a view matrix from the camera matrix.
+    const view = m4.inverse(camera);
+
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
+    };
+
+    gl.useProgram(meshProgramInfo.program);
+
+    // calls gl.uniform
+    twgl.setUniforms(meshProgramInfo, sharedUniforms);
+
+    // compute the world matrix once since all parts
+    // are at the same space.
+    let u_world = m4.yRotation(-time);
+    u_world = m4.translate(u_world, ...objOffset);
+
+
+    //---- ANNA: DRAW primeiro objeto----
+    for (const {bufferInfo, vao, material} of parts) {
+      // set the attributes for this part.
+      gl.bindVertexArray(vao);
+      // calls gl.uniform
+      twgl.setUniforms(meshProgramInfo, {
+        u_world,
+        u_diffuse: material.u_diffuse,
+      });
+      // calls gl.drawArrays or gl.drawElements
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
+
+
+    //----ANNA: Colocar os objetos em diferentes posições:----
+
+    //lighthouse
+    // u_world normal.
+
+    //turtle
+    turtle += 0.06; //velocidade do movimento devido à diferença do seno. 1 = frenético.
+    var u_worldEscalado1;
+    var  u_worldTransladado1;
+
+    function transladaTurtle (turtle){
+      u_worldEscalado1 = m4.scale(u_world, 0.5, 0.5, 0.5);
+      turtle = Math.sin(turtle)* 15;
+      u_worldTransladado1 = m4.translate(u_worldEscalado1, -12, 15, turtle);
+      return u_worldTransladado1;
+    }
+
+    //shark
+    shark += 0.06;
+    var u_worldEscalado2;
+    var  u_worldTransladado2;
+
+    function transladaShark (shark){
+      u_worldEscalado2 = m4.scale(u_world, 0.03, 0.03, 0.03);
+      shark = (-Math.sin(shark)) * 200; //o - p/ eles me movimentarem de forma oposta e ficar bonito.
+      u_worldTransladado2 = m4.translate(u_worldEscalado2, 250, 200, shark); //-200 - 200 
+      return u_worldTransladado2;
+    }
+
+
+    //----ANNA: DRAW segundo objeto:----
+
+    for (const { bufferInfo, vao, material } of PartsSecondOBJ) {
+
+      //movimenta obj
+      u_worldTransladado1 = transladaTurtle(turtle);
+
+
+      // set the attributes for this part.
+      gl.bindVertexArray(vao);
+      // calls gl.uniform
+      twgl.setUniforms(meshProgramInfo, {
+        u_world: u_worldTransladado1,
+        u_diffuse: material.u_diffuse,
+      });
+      // calls gl.drawArrays or gl.drawElements
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
+
+    //----ANNA: DRAW terceiro objeto----
+    for (const { bufferInfo, vao, material } of PartsThirdOBJ) {
+
+      //movimenta obj
+      u_worldTransladado2 = transladaShark(shark);
+      
+
+      // set the attributes for this part.
+      gl.bindVertexArray(vao);
+      // calls gl.uniform
+      twgl.setUniforms(meshProgramInfo, {
+        u_world: u_worldTransladado2,
+        u_diffuse: material.u_diffuse,
+      });
+      // calls gl.drawArrays or gl.drawElements
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
---
+++ site/index.js
@@ -0,0 +1,119 @@
+﻿
+//<!--<script src = "index.js" ></script> -->
+//-----------------------------------------------------------
+
+//buttons, background, text:
+document.getElementsByTagName("body")[0].style.cursor = "blue.png, auto";
+
+
+function imBlue(){
+    //pinpoint box class
+    document.querySelector('.box').style.color = 'blue';
+
+    var dab = document.getElementById("dab");
+    dab.style.color = 'blue'
+    dab.innerHTML = "DABADEE DABADI";
+    document.body.style.backgroundImage = "url('back1.gif')";
+}
+
+// increase text size
+function bigger(id, pxInc){
+    txt = document.getElementById(id); //grab button
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size'); //parse button
+    sizeNow = parseFloat(style); //current size
+    txt.style.fontSize = (sizeNow + pxInc) + 'px'; //inc
+}
+//gif:
+document.getElementById( 'naomi' ).style.cursor = "pointer";
+document.getElementById( 'naomi' ).style.position = "absolute";
+document.getElementById( 'naomi' ).style.top = "110px";
+document.getElementById( 'naomi' ).style.left = "900px";
+
+
+//-----------------------------------------------------------------------
+//ESTUDO:
+
+// <!-- <h1>Hello World</h1> -->
+
+//coment
+console.log( 'Hello World' ); //this is a statement
+
+//print on console:
+let nome = "Anna"
+console.log( nome ); 
+
+//COnstants:
+const FavoriteColor = "Blue";
+
+//primitive types:
+let namee = "anna";
+let age = "22"; // n tem diferenca entre float e inteiro
+let pretty = true;
+let memory = undefined;
+let senseOfDirection = null; //clear value of variable
+
+
+typeof nome
+
+
+// declare person object:
+let person = 
+{
+    namee: "anna",
+    age: 22
+};
+
+console.log( person );
+
+//dot notation:
+person.namee = "Agmo";
+//bracket notation:
+person["namee"] = "Anna";
+
+//Arrays:
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "green"; //append
+selectedColors[3] = "#0049FD";
+
+console.log(selectedColors);
+console.log(selectedColors[0]);
+//console.log(selectedColors.) varios atributos
+console.log(selectedColors.length);
+
+
+//functions
+function greet(Nome, idade) {
+    console.log("Hello " + Nome + idade);
+}
+
+greet("Anna", 22);
+
+
+function square(number) 
+{
+    return number * number
+}
+
+let number = square(2);
+console.log(number);
+
+//------------------------------------
+// COISAS Q EU TIREI:
+
+
+// NO BODY DO CSS:
+
+  /*<div id = "bingbing" class="ascii-art" >    
+      ╭――――――――――――――――――╮
+      │                  │
+      │    BING BING     │
+      │                  │
+      ╰―――――――――――――――   ╯
+                       \|
+    </div>
+    */
+     /*<img id="bing" src="bing.gif" onclick="HTMLredirect();">*/
+
+
+// NO JS:
+//document.getElementById( 'bingbing' ).style.position = "absolute";
